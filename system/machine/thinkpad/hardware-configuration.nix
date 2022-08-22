a905dd8d55c51105cdf6a4ceb06196d1ea447e34
Ogglord <oggelito@gmail.com>
2022-08-22 19:20:15 +0200

    hardware tweak

--- system/machine/thinkpad/hardware-configuration.nix
+++ system/machine/thinkpad/hardware-configuration.nix
@@ -4,9 +4,9 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  # imports =
-  #   [ (modulesPath + "/installer/scan/not-detected.nix")
-  #   ];
+   imports =
+     [ (modulesPath + "/installer/scan/not-detected.nix")
+     ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ "i915" ]; ## manually added
@@ -23,9 +23,10 @@
       fsType = "vfat";
     };
 
-  #swapDevices = [ ];
+  swapDevices = [ ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "performance";
+  hardware.enableRedistributableFirmware = true;
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
