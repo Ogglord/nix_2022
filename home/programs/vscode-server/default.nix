f1d2ed208d7d08d377740a4b5e81309d55eab9f0
Ogglord <oggelito@gmail.com>
2022-08-22 20:27:43 +0200

    vscode #2

--- home/programs/vscode-server/default.nix
+++ home/programs/vscode-server/default.nix
@@ -1,7 +1,7 @@
 { pkgs, lib, specialArgs, ... }:
 
 {
-  import = [
+  imports = [
     "${fetchTarball { url = "https://github.com/msteen/nixos-vscode-server/tarball/master"; sha256 = "1dr3v3mlf61nrs3f3d9qx74y8v5jihkk8wd1li4sglx22aqh4xf6"; } }/modules/vscode-server/home.nix"
   ];
 
